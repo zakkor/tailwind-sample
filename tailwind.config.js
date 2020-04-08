0b8059c37c0cf3aa3f8902d1a25bbbda515d7656
Zakkor <edward.partenie@gmail.com>
2020-04-09 00:09:36 +0300

    add stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   theme: {
+    fontFamily: {
+      sans: 'Poppins, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"'
+    },
     extend: {},
   },
   variants: {},
