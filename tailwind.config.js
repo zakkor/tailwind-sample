57f8133712e12740d7570196e8078b5dd9240e80
zakkor <edward.partenie@gmail.com>
2020-04-09 01:51:12 +0300

    sample 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,27 @@ module.exports = {
     fontFamily: {
       sans: 'Poppins, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"'
     },
-    extend: {},
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1400px',
+      // => @media (min-width: 1280px) { ... }
+    },
+    extend: {
+      colors: {
+        green: '#6AC75A',
+        indigo: '#783BFF',
+        'gray-muted': '#848485',
+        'gray-light': '#F8F8F8',
+      },
+    },
   },
   variants: {},
   plugins: [],
